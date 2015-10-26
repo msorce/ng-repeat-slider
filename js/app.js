69529e9583994e52db125a5d9336907d4248c40f
Sorce <HIS93425@hke.local>
2015-10-26 15:36:29 -0700

    added an http request inside the directive, to get slider data

--- js/app.js
+++ js/app.js
@@ -1,38 +1,26 @@
 var app = angular.module('app', []);
 app
-  .directive('slickSlider', function() {
+  .directive('slickSlider', ['$http', function($http) {
     return {
       restrict: 'A',
       link: function(scope, el, attrs) {
-        scope.slides = [
-          "Slide 1",
-          "Slide 2",
-          "Slide 3",
-          "Slide 4",
-          "Slide 5",
-          "Slide 6",
-          "Slide 7",
-          "Slide 8",
-          "Slide 9",
-          "Slide 10",
-          "Slide 11",
-          "Slide 12",
-          "Slide 13",
-          "Slide 14",
-          "Slide 15",
-          "Slide 16",
-          "Slide 17",
-          "Slide 18",
-          "Slide 19",
-          "Slide 20"
-        ];
         setTimeout(function() {
           $(el).slick(scope.$eval(attrs.slickSlider));
         }, 0)
-      }
+      },
+      controller: function($scope) {
+        $http({
+          method: 'GET',
+          url: '/data/slide-data.json'
+        }).then(function(result) {
+          $scope.slides = result.data.slides;
+          console.log($scope.slides);
+        }, function(result) {
+          alert("Error: No data returned");
+        });
+      },
     };
-  });
-
+  }]);
 
 app
   .controller('footerCtrl', ['$scope', function($scope) {
